a15ca228e2882de5eec033fe3e05cdf13fdb0da7
User <User@W10x64>
2018-10-24 12:06:48 +0530

    gt

--- src/practice/Demo.java
+++ src/practice/Demo.java
@@ -1,7 +1,12 @@
 package practice;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class Demo {
-public static void main(String[] args) {
-	System.out.println("Hi");
+
+@Test
+public  void testA() {
+	Reporter.log("testa",true);
 }
 }
